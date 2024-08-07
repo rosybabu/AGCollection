eefb9714f78f7f257d848cac75243c18bad58c79
rosybabu <rosybabu8@gmail.com>
2024-08-07 11:45:51 +0400

    js issue

--- dev/Arabic/js/main.js
+++ dev/Arabic/js/main.js
@@ -271,7 +271,10 @@ var main = {
 
 $(document).ready(function () {
   $(".html5-main-video").css("width", "100% !important");
-
+  window.onload = function () {
+    var element = document.getElementById('main_video');
+    element.muted = "muted";
+}
   AOS.init();
   main.humburger();
   main.slider();

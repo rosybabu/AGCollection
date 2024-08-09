d4563175854fee8c5aee9069cf969ba6ce335a6b
rosybabu <rosybabu8@gmail.com>
2024-08-09 19:05:11 +0400

    cr changes

--- dev/js/main.js
+++ dev/js/main.js
@@ -299,15 +299,16 @@ $(document).ready(function () {
     var element = document.getElementById('main_video');
     element.muted = "muted";
 }
-$('.form-control').on('focus blur change', function (e) {
+
+$('select').on('focus blur change', function (e) {
 	var $currEl = $(this);
   
   if($currEl.is('select')) {
   	if($currEl.val() === $("option:first", $currEl).val()) {
-    	$('.control-label', $currEl.parent()).animate({opacity: 0}, 240);
+    	$('.select-label', $currEl.parent()).animate({opacity: 0}, 240);
       $currEl.parent().removeClass('focused');
     } else {
-    	$('.control-label', $currEl.parent()).css({opacity: 1});
+    	$('.select-label', $currEl.parent()).css({opacity: 1});
     	$currEl.parents('.select_group').toggleClass('focused', ((e.type === 'focus' || this.value.length > 0) && ($currEl.val() !== $("option:first", $currEl).val())));
     }
   } else {
